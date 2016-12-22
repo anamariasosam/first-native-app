b4adcf4926198603ef9dbe5ca9754d38a3e5106a
Ana Maria Sosa <anamariasosam@gmail.com>
2016-12-22 18:25:30 -0500

    create albumdetail component

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+} from 'react-native';
+
+import Card from './Card';
+import CardSection from './CardSection';
+
+const AlbumDetail = ({ album }) => {
+  const {
+    title,
+    artist,
+    thumbnail_image,
+    image,
+  } = album;
+
+  const {
+    thumbnailStyle,
+    thumbnailContainer,
+    headerContent,
+    headerText,
+    songImage,
+  } = styles;
+
+  return (
+    <Card>
+      <CardSection>
+        <View style={thumbnailContainer}>
+          <Image
+            source={{ uri: thumbnail_image }}
+            style={thumbnailStyle}
+          />
+        </View>
+        <View style={headerContent}>
+          <Text style={headerText}>{ title }</Text>
+          <Text>{ artist }</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image source={{ uri: image }} style={songImage} />
+      </CardSection>
+    </Card>
+  );
+};
+
+const styles = StyleSheet.create({
+  headerContent: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    marginLeft: 10,
+  },
+  headerText: {
+    fontSize: 18,
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50,
+  },
+  thumbnailContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  songImage: {
+    height: 300,
+    width: null,
+    flex: 1,
+  }
+});
+
+
+export default AlbumDetail;
