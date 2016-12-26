a48fb45d72798e3547aeb8b844706735466431a6
Ana Maria Sosa <anamariasosam@gmail.com>
2016-12-26 16:58:39 -0500

    add flex to the main view and scrollview to the list

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {
-  View,
+  ScrollView,
 } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
@@ -28,9 +28,9 @@ class AlbumList extends Component {
   render() {
     console.log(this.state.albums);
     return (
-      <View>
+      <ScrollView>
         { this.renderAlbums() }
-      </View>
+      </ScrollView>
     );
   }
 }
